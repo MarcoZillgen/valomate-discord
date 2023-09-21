5d3d30578f9cf6f729ae6b769d8e512e068e63a1
Käkäsh <marco.zillgen23@gmail.com>
2023-09-21 10:46:08 +0200

    start

---
+++ src/Bot.ts
@@ -0,0 +1,15 @@
+import { Client } from 'discord.js'
+import ready from './listeners/ready'
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('Bot is starting...')
+
+const client = new Client({
+  intents: []
+})
+
+ready(client)
+
+client.login(process.env.BOT_TOKEN)
+
